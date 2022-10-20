001f050f0314750ae83621a1f1460aff4c06cdde
Vignesh Reddy Julakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2022-10-20 19:23:32 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route ,Routes} from 'react-router-dom';
+import Home from './Components/Home';
+import Attendance from './Components/Attendance';
+import Section from './Components/Section.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+<Routes>
+
+  <Route path='/' element={<Home></Home>}></Route>
+  <Route path='/attendance' element={<Attendance></Attendance>}></Route>
+  <Route path='/section' element={<Section></Section>}></Route>
+  </Routes>
+    
+    
+    </>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+export default function 
+() {
+  let Navigate=useNavigate();
+  return (
+    <div id='home_body'>
+      <h1 id='boom_welcome' >Boom!!</h1>
+      <div className='d-flex justify-content-center my-3'>
+        <Link to="/attendance" id='click_me_for_att'>Click Me For Your Attendance</Link><br></br>
+        </div>
+
+          <p className='d-flex justify-content-center' id='get_entire'>Get Entire Section Attendance</p>
+        <div id='sections_but' className='container my-3' >
+          <div className='container'>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_a"}})}}>CSE A</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_b"}})}}>CSE B</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_c"}})}}>CSE C</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_d"}})}}>CSE D</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_e"}})}}>CSE E</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_f"}})}}>CSE F</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"cse_g"}})}}>CSE G</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"aiml_a"}})}}>AIML A</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"aiml_b"}})}}>AIML B</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"it_a"}})}}>IT A</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"it_b"}})}}>IT B</button>
+        <button className='mx-2 but_home_sec' onClick={()=>{Navigate("/section",{state:{"sec":"ds_a"}})}}>DS A</button>
+        </div>
+        </div>
+    </div>
+  )
+}
